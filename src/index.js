cbac6f7f2fc7319abd8ad61af2c58bfcce793d0c
StanislavBadyoukov <stanislav.badyoukov@mail.ru>
2022-02-24 22:18:34 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let openBrackets = [];
+  let pairsOfBrackets = new Object();
+  for (let i = 0; i < bracketsConfig.length; i++) {
+      pairsOfBrackets[bracketsConfig[i][1]] = bracketsConfig[i][0];
+      openBrackets.push(bracketsConfig[i][0]);
+  }
+  for (let i = 0; i < str.length; i++) {
+      if (openBrackets.includes(str[i])) {
+          if (pairsOfBrackets[str[i]] === str[i]) {
+              if (stack.length !== 0 && stack[stack.length - 1] === str[i]) { stack.pop() }
+              else { stack.push(str[i]) }
+          }
+          else { stack.push(str[i]) }
+      }
+      else {
+          if (stack.length === 0) { return false }
+          if (pairsOfBrackets[str[i]] === stack[stack.length - 1]) { stack.pop() }
+          else { return false }
+      }
+  }
+  return stack.length === 0;
 }
